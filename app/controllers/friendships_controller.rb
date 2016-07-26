b774b1be3bbac0628ca586b2dbac7ecc280f1581
peterbuss <peter.buss@hispeed.ch>
2016-07-26 22:16:57 +0000

    implement showing friend workouts

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,6 +7,11 @@ class FriendshipsController < ApplicationController
     redirect_to root_path
   end
   
+  def show
+    @friend = Friendship.find(params[:id]).friend
+    @exercises = @friend.exercises.all
+  end
+  
   private
   
   def friendship_params
