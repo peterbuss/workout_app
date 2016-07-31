637335b5b200c169a139873f23b70419401f948b
peterbuss <peter.buss@hispeed.ch>
2016-07-31 21:46:31 +0000

    Changed friend migration file to use add_foriegn_key

--- db/migrate/20160721222933_create_friendships.rb
+++ db/migrate/20160721222933_create_friendships.rb
@@ -1,10 +1,12 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+      t.references :user, index: true
+      t.references :friend, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :friendships, :users
+    add_foreign_key :friendships, :friends    
   end
 end
